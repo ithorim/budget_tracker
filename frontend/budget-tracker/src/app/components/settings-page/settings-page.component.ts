c52c535181cc0855e4899288749bca9209b0b585
ithorim <tihomir.rajicic@gmail.com>
2025-02-09 18:30:03 +0100

    changed px values to rem, added validation of password on input fields

--- frontend/budget-tracker/src/app/components/settings-page/settings-page.component.ts
+++ frontend/budget-tracker/src/app/components/settings-page/settings-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/auth.models';
 import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
@@ -19,6 +20,7 @@ export class SettingsPageComponent implements OnInit {
     password: ''
   }
 
+  currentPassword: string = '';
   newPassword: string = '';
   confirmPassword: string = '';
 
@@ -28,13 +30,42 @@ export class SettingsPageComponent implements OnInit {
     });
   }
 
+  isCurrentPasswordValid(): boolean {
+    return this.currentPassword.length > 0;
+  }
+
+  areNewPasswordsValid(): boolean {
+    return this.newPassword.length > 0 && 
+           this.confirmPassword.length > 0 && 
+           this.newPassword === this.confirmPassword &&
+           this.newPassword !== this.currentPassword;
+  }
+
+  isPasswordChangeValid(): boolean {
+    return this.isCurrentPasswordValid() && this.areNewPasswordsValid();
+  }
+
+  showPasswordMismatchError(): boolean {
+    return this.confirmPassword.length > 0 && 
+           this.newPassword !== this.confirmPassword;
+  }
+
   updateProfile() {
     this.successMessage = 'Profile updated successfully';
     this.errorMessage = '';
   }
 
   updatePassword() {
+    if (!this.isPasswordChangeValid()) {
+      this.errorMessage = 'Please check your password entries';
+      return;
+    }
+    
     this.successMessage = 'Password updated successfully';
     this.errorMessage = '';
+    
+    this.currentPassword = '';
+    this.newPassword = '';
+    this.confirmPassword = '';
   }
 }
